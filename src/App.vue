0c9e0ed937cfc2b6a8fe552922a9b4553b9d7c21
MattPChoy <matthewpchoy@gmail.com>
2025-06-25 20:37:53 +1000

    Add a footer with a link to the github repository.

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,9 @@
 
   </NavBar>
   <InputExamples/>
+  <div class="footer-container">
+    Created by Matt Choy | &nbsp; <a href="http://github.com/MattPChoy/CustomCompoments">Custom Components on GitHub</a>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -12,4 +15,13 @@ import NavBar from "./components/Layout/NavBar.vue";
 </script>
 
 <style scoped>
+.footer-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+
+  background-color: var(--bg-main-10);
+  padding: var(--space-8) var(--space-2) var(--space-4) var(--space-2);
+}
 </style>
