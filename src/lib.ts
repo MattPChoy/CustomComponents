22e4de385e6fdd2ed909040d376b73667a0be3e2
MattPChoy <matthewpchoy@gmail.com>
2025-06-29 19:25:14 +1000

    Try to get build working

---
+++ src/lib.ts
@@ -0,0 +1,15 @@
+import CFileUpload from "./components/Inputs/CFileUpload.vue";
+import CNumberInput from "./components/Inputs/CNumberInput.vue";
+import CTextInput from "./components/Inputs/CTextInput.vue";
+import CToggleInput from "./components/Inputs/CToggleInput.vue";
+import CTable from "./components/Layout/CTable.vue";
+import type {PaginationParams, ColumnOptions, TableOptions} from "./components/Layout/Table/TableOptions.ts";
+import NavBar from "./components/Layout/NavBar.vue";
+
+// Exports for NPM package
+export {
+    CFileUpload, CNumberInput, CTextInput, CToggleInput,
+
+    CTable, NavBar,
+};
+export type { PaginationParams, TableOptions,ColumnOptions};
---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from "path";
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [vue()],
+    base: "/CustomComponents/",
+    server: {
+        port: 4000
+    },
+    build: {
+        lib: {
+            // The file that is loaded when someone imports the plugin into the app
+            entry: resolve(__dirname, "src/lib.ts"),
+            name: "MattCustomComponents",
+            fileName: "matt-custom-components"
+        },
+        rollupOptions: {
+            // make sure to externalize deps that shouldn't be bundled
+            // into your library
+            external: ["vue"],
+            output: {
+                // Provide global variables to use in the UMD build
+                // for externalized deps
+                globals: {
+                    vue: "Vue",
+                },
+            },
+        },
+    },
+});
