082d9fd8df341a59066deacebef6eed13ea2e527
MattPChoy <matthewpchoy@gmail.com>
2025-07-12 11:00:45 +1000

    Fix docs

---
+++ vite.config.docs.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from "path";
+import dts from 'vite-plugin-dts'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+  ],
+  base: "/CustomComponents/",
+  server: {
+    port: 4000
+  },
+});
