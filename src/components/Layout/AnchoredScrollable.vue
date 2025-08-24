ec5faf988423d812524b0d81e8482a872a77381c
MattPChoy <matthewpchoy@gmail.com>
2025-08-24 17:56:16 +1000

    WIP: AnchoredScrollable.vue

---
+++ src/components/Layout/AnchoredScrollable.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="anchored-scrollable-container">
+    <div class="anchored-scrollable-header">
+      <slot name="header"/>
+    </div>
+    
+    <div class="anchored-scrollable-content">
+      <slot name="content"/>
+    </div>
+    
+    <div class="anchored-scrollable-footer">
+      <slot name="footer"/>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+.anchored-scrollable-container {
+  display: flex;
+  flex-direction: column;
+}
+
+.anchored-scrollable-content {
+  overflow-y: auto;
+  flex: 1;
+}
+</style>
